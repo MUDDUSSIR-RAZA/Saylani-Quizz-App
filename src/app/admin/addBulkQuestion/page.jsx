18b47adaf91f9891f166227ab2e7e2f3cda6c8fb
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-06-30 21:47:10 +0500

    Create page.jsx

---
+++ src/app/admin/addBulkQuestion/page.jsx
@@ -0,0 +1,18 @@
+import AdminNavbar from "@/components/AdminNavbar";
+import BulkUploadQuestions from "@/components/admin/BulkUploadQuestions";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div className="overflow-hidden w-dvw h-dvh">
+        <AdminNavbar />
+        <main className="items-center justify-between p-24 -z-30">
+          <BulkUploadQuestions />
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default page;
