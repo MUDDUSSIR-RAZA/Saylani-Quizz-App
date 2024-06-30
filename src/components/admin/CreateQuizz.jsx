407d3f08b6b970a312470f202f5763bea9e2b25e
MUDDUSSIR AWAN <126598592+MUDDUSSIR-RAZA@users.noreply.github.com>
2024-06-30 22:26:01 +0500

    Update CreateQuizz.jsx

--- src/components/admin/CreateQuizz.jsx
+++ src/components/admin/CreateQuizz.jsx
@@ -55,15 +55,15 @@ const CreateQuizz = () => {
       <div>
         <Toaster />
       </div>
-      <section className="bg-slate-50 dark:bg-gray-900 w-vwh h-dvh flex">
+      <section className="bg-slate-50 w-vwh h-dvh flex">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 w-10/12 sm:px-0">
-          <div className="w-10/12 backdrop-blur-sm bg-[#ccc5c56d] rounded-lg shadow dark:border md:mt-0 sm:w-full xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-10/12 backdrop-blur-sm bg-[#ccc5c56d] rounded-lg shado md:mt-0 sm:w-full xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-2xl font-bold leading-tight tracking-tight text-gray-900 md:text-xl dark:text-white">
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400 relative">
+              <h1 className="text-2xl font-bold leading-tight tracking-tight text-gray-900 md:text-xl">
+                <p className="text-sm font-light text-gray-500 relative">
                   <a
                     href="/auth/signup"
-                    className="font-medium text-black hover:underline dark:white absolute right-5"
+                    className="font-medium text-black hover:underline absolute right-5"
                   >
                     X
                   </a>
@@ -74,7 +74,7 @@ const CreateQuizz = () => {
                 <div>
                   <label
                     htmlFor="quiz"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900"
                   >
                     Your email
                   </label>
@@ -82,7 +82,7 @@ const CreateQuizz = () => {
                     type="text"
                     name="quiz"
                     id="quiz"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                     placeholder="Course Name"
                     required
                     autoComplete="off"
@@ -102,7 +102,7 @@ const CreateQuizz = () => {
                   className={`w-full text-white flex justify-center  ${
                     loading
                       ? "bg-[#22659c]"
-                      : "bg-primary-600 dark:bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                      : "bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 "
                   }  font-medium rounded-lg text-sm px-5 py-2.5 text-center  `}
                 >
                   {loading ? <Creating /> : "Create Quiz"}
