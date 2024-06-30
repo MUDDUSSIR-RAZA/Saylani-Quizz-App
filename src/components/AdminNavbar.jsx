57c92780e5bd1fde188b47e87ea3a8c2bd30cdf2
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-06-30 21:47:21 +0500

    Update AdminNavbar.jsx

--- src/components/AdminNavbar.jsx
+++ src/components/AdminNavbar.jsx
@@ -90,9 +90,19 @@ const AdminNavbar = () => {
   const links = [
     {
       id: 1,
-      name: "add quizz",
-      link: "addquizz",
-    }
+      link: "/admin/addQuiz",
+      name: "Add Quiz",
+    },
+    {
+      id: 1,
+      link: "/admin/addQuestion",
+      name: "Add Question",
+    },
+    {
+      id: 1,
+      link: "/admin/addBulkQuestion",
+      name: "Add Bulk Questions",
+    },
   ];
 
   const handleLogout = async () => {
@@ -108,7 +118,7 @@ const AdminNavbar = () => {
 
   return (
     <nav
-      className={`transition-all duration-500 ease-in-out w-full  ${
+      className={`transition-all duration-500 ease-in-out w-full z-[999]  ${
         isSticky ? "fixed top-6 w-full" : "absolute"
       }`}
     >
@@ -118,7 +128,6 @@ const AdminNavbar = () => {
         } ${isSticky ? "w-[92%] rounded-full   " : ""}`}
       >
         <div className="relative flex items-center justify-between h-16 w-full">
-          {/* //Toggle Buttons */}
           <div
             onClick={handleToggle}
             className="cursor-pointer absolute inset-y-0 right-5 md:flex items-center hidden "
@@ -126,9 +135,8 @@ const AdminNavbar = () => {
             {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
           </div>
 
-          {/* md:items-stretch md:justify-between  */}
+
           <div className="flex items-center justify-between w-full">
-            {/* Navbar branding/logo */}
             <div className="openNavbarLogo flex-shrink-0 flex items-center md:ml-7 ml-3">
               <a href="/" className="text-gray-900 text-lg font-bold ">
                 Logo
@@ -136,14 +144,13 @@ const AdminNavbar = () => {
             </div>
 
             <div className="flex items-center justify-around">
-              {/* Navbar links */}
-              <div className="md:hidden block mr-2 text-2xl font-semibold text-center">
+              <div className="md:hidden block mr-2 text-xl font-semibold text-center ">
                 <div className="flex justify-around capitalize">
-                  {links.map(({ name , link, id }) => (
+                  {links.map(({ link, id ,name }) => (
                     <Link
                       key={id}
                       href={link}
-                      className="openNavbarLinks text-[#2c4a4f] hover:text-gray-700 ml-3 minLg:ml-11"
+                      className="openNavbarLinks text-[#2c4a4f] hover:text-gray-700 ml-3 minLg:ml-11 bg-[#ffffff72] p-2 rounded-full px-4"
                       id="openNavbarLinksRef"
                     >
                       {name}
@@ -153,7 +160,7 @@ const AdminNavbar = () => {
               </div>
               <button
                 onClick={handleLogout}
-                className="openNavbarLinks bg-[#ffffff72] p-2 rounded-full text-lg text-center minLg:mx-5 mr-16 minMd:mr-0 md:hidden"
+                className="openNavbarLinks bg-[#ffffff72] p-2 rounded-full text-lg text-center minLg:mx-5 mr-16 minMd:mr-0 md:hidden px-4"
               >
                 Logout
               </button>
@@ -178,14 +185,14 @@ const AdminNavbar = () => {
 
         <div className="h-5/6 w-full text-3xl font-semibold text-center absolute top-10  mx-auto">
           <div className="flex flex-col justify-around text-center my-auto">
-            {links.map(({ link, id }) => (
+            {links.map(({ link, id ,name }) => (
               <a
                 key={id}
-                href="/auth/signup"
+                href={link}
                 className="closeNavbarLinks text-[#2c4a4f] hover:text-gray-700  my-14 mx-14 capitalize"
                 onClick={handleToggle}
               >
-                {link}
+                {name}
               </a>
             ))}
             <button
