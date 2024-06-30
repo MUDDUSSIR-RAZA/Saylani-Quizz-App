6babd5d7cebb3fb045e1963c50fd28bdf32290af
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-06-30 21:47:34 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -90,35 +90,30 @@ const Navbar = () => {
   const links = [
     {
       id: 1,
-      link: "home",
+      link: "/addQuiz",
+      name: "Add Quiz",
     },
     {
-      id: 2,
-      link: "about",
+      id: 1,
+      link: "/addQuestion",
+      name: "Add Question",
     },
     {
-      id: 3,
-      link: "portfolio",
-    },
-    {
-      id: 4,
-      link: "experience",
-    },
-    {
-      id: 5,
-      link: "contact",
+      id: 1,
+      link: "/addBulkQuestion",
+      name: "Add Bulk Questions",
     },
   ];
 
   const handleLogout = async () => {
-    try {
-      await axios.post("/api/user/logout");
-      router.push("/auth/signup")
-      return;
-    } catch (error) {
-      console.error(error.response.data);
-      return;
-    }
+    // try {
+    //   await axios.post("/api/user/logout");
+    //   router.push("/auth/signup")
+    //   return;
+    // } catch (error) {
+    //   console.error(error.response.data);
+    //   return;
+    // }
   };
 
   return (
@@ -133,7 +128,6 @@ const Navbar = () => {
         } ${isSticky ? "w-[92%] rounded-full   " : ""}`}
       >
         <div className="relative flex items-center justify-between h-16 w-full">
-          {/* //Toggle Buttons */}
           <div
             onClick={handleToggle}
             className="cursor-pointer absolute inset-y-0 right-5 md:flex items-center hidden "
@@ -141,9 +135,8 @@ const Navbar = () => {
             {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
           </div>
 
-          {/* md:items-stretch md:justify-between  */}
+
           <div className="flex items-center justify-between w-full">
-            {/* Navbar branding/logo */}
             <div className="openNavbarLogo flex-shrink-0 flex items-center md:ml-7 ml-3">
               <a href="/" className="text-gray-900 text-lg font-bold ">
                 Logo
@@ -151,24 +144,23 @@ const Navbar = () => {
             </div>
 
             <div className="flex items-center justify-around">
-              {/* Navbar links */}
-              <div className="md:hidden block mr-2 text-2xl font-semibold text-center">
+              <div className="md:hidden block mr-2 text-xl font-semibold text-center ">
                 <div className="flex justify-around capitalize">
-                  {links.map(({ link, id }) => (
+                  {links.map(({ link, id ,name }) => (
                     <Link
                       key={id}
-                      href="/auth/signup"
-                      className="openNavbarLinks text-[#2c4a4f] hover:text-gray-700 ml-3 minLg:ml-11"
+                      href={link}
+                      className="openNavbarLinks text-[#2c4a4f] hover:text-gray-700 ml-3 minLg:ml-11 bg-[#ffffff72] p-2 rounded-full px-4"
                       id="openNavbarLinksRef"
                     >
-                      {link}
+                      {name}
                     </Link>
                   ))}
                 </div>
               </div>
               <button
                 onClick={handleLogout}
-                className="openNavbarLinks bg-[#ffffff72] p-2 rounded-full text-lg text-center minLg:mx-5 mr-16 minMd:mr-0 md:hidden"
+                className="openNavbarLinks bg-[#ffffff72] p-2 rounded-full text-lg text-center minLg:mx-5 mr-16 minMd:mr-0 md:hidden px-4"
               >
                 Logout
               </button>
@@ -193,14 +185,14 @@ const Navbar = () => {
 
         <div className="h-5/6 w-full text-3xl font-semibold text-center absolute top-10  mx-auto">
           <div className="flex flex-col justify-around text-center my-auto">
-            {links.map(({ link, id }) => (
+            {links.map(({ link, id ,name }) => (
               <a
                 key={id}
-                href="/auth/signup"
+                href={link}
                 className="closeNavbarLinks text-[#2c4a4f] hover:text-gray-700  my-14 mx-14 capitalize"
                 onClick={handleToggle}
               >
-                {link}
+                {name}
               </a>
             ))}
             <button
