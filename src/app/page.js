c58d390c8fa5f2f1f7fda14a3e2f1577b44fbeee
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-06-30 21:47:15 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,109 +1,12 @@
 import Navbar from "@/components/Navbar";
+import CourseList from "@/components/admin/CourseList";
 
 export default function Home() {
+  
   return (<>
   <Navbar/>
    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
-     <h1>safdsf</h1>
+    <CourseList  />
     </main>
   </>
    
