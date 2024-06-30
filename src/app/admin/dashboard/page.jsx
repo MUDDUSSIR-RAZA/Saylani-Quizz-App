fe0c3e8cb06aa50c700be98c79083ad9ea01a1b1
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-06-30 21:47:25 +0500

    Update page.jsx

--- src/app/admin/dashboard/page.jsx
+++ src/app/admin/dashboard/page.jsx
@@ -1,12 +1,14 @@
 import AdminNavbar from '@/components/AdminNavbar'
+import CourseList from '@/components/admin/CourseList'
 import React from 'react'
 
 const dashboard = () => {
   return (
     <>
     <AdminNavbar />
-
-    
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <CourseList />
+    </main>
     </>
   )
 }
