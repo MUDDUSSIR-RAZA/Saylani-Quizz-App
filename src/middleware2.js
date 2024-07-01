c9295fa7c43b68aea79b81d3d807d95c74c7e6d1
MUDDUSSIR AWAN <126598592+MUDDUSSIR-RAZA@users.noreply.github.com>
2024-07-01 23:13:27 +0500

    Update middleware2.js

--- src/middleware2.js
+++ src/middleware2.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-// This fon mare`sync` ifsing `await` iside
+// This fon mare`sync` ifsing `await` isid
 export function middleware(request) {
     const path = request.nextUrl.pathname
 
