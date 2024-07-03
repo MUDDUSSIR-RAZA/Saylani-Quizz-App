099045a7e708a5e221976bcdb76b411022c3ed19
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-07-03 20:16:51 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`bg-slate-50 ${inter.className}`}>
+      <body className={`bg-slate-50  bg-fixed bg-center bg-contain  ${inter.className} bg-[url('https://th.bing.com/th/id/OIP.NkwEpqJktRs1GVY_474xRQHaHa?w=184&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7')]`} >
         {children}
       </body>
     </html>
