59142fc77c571266f167e2da2dac7ca73a5b3831
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-07-03 20:16:54 +0500

    Update AddQuestion.jsx

--- src/components/admin/AddQuestion.jsx
+++ src/components/admin/AddQuestion.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Adding from "../InPageLoader/Adding";
 
-const AddQuestion = () => {    
+const AddQuestion = () => {
   const [loading, setLoading] = useState(false);
   const [courses, setCourses] = useState([
     { _id: "60d21b4667d0d8992e610c85", course_name: "Mathematics 101" },
@@ -40,45 +40,51 @@ const AddQuestion = () => {
     e.preventDefault();
     console.log(correctOptionIndex, timeLimit);
     try {
-        setLoading(true);
-    //   const response = await axios.post(
-    //     `/api/courses/${selectedCourse}/add-question`,
-    //     {
-    //       question_text: questionText,
-    //       options,
-    //       correct_option_index: correctOptionIndex,
-    //       time_limit: timeLimit,
-    //     }
-    //   );
-    // setLoading(false);
+      setLoading(true);
+      //   const response = await axios.post(
+      //     `/api/courses/${selectedCourse}/add-question`,
+      //     {
+      //       question_text: questionText,
+      //       options,
+      //       correct_option_index: correctOptionIndex,
+      //       time_limit: timeLimit,
+      //     }
+      //   );
+      // setLoading(false);
 
-    //   if (response.data.success) {
-    //     // router.push('/admin'); // Redirect to admin dashboard or another page
-    //   }
+      //   if (response.data.success) {
+      //     // router.push('/admin'); // Redirect to admin dashboard or another page
+      //   }
     } catch (error) {
-        setLoading(false);
+      setLoading(false);
       console.error("Error adding question:", error);
     }
   };
 
   return (
-    <section className="bg-slate-50 w-vwh h-dvh flex -z-99">
+    <section className="w-vwh h-dvh flex -z-99">
       <div className="flex items-baseline justify-center my-[20px] h-[90%] px-6 py-8 mx-auto md:h-screen lg:py-0 w-10/12 sm:px-0">
-        <div className="w-10/12 backdrop-blur-sm bg-[#ccc5c56d] rounded-lg shadow md:mt-0 sm:w-full xl:p-0">
+        <div className="w-9/12 backdrop-blur-xl bg-bgColor rounded-lg shadow-2xl md:mt-0 sm:w-full xl:p-0">
           <div className="p-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl mb-7">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 mb-7 relative md:text-xl">
+              <a
+                href="/admin/dashboard"
+                className="font-medium text-black hover:underline absolute right-5 md:right-3"
+              >
+                X
+              </a>
               Add a New Question
             </h1>
             <form onSubmit={handleFormSubmit}>
               <label htmlFor="courseSelect" className="mr-2 mb-3">
-                <b>Select Course:</b>
+                Select Course:
               </label>
               <select
                 id="courseSelect"
                 value={selectedCourse}
                 onChange={(e) => setSelectedCourse(e.target.value)}
                 required
-                className="border-2 border-black rounded-md"
+                className="bg-bgColor font-bold text-slate-800 border border-gray-300 rounded-sm"
               >
                 <option value="">Select a course</option>
                 {courses.map((course) => (
@@ -91,26 +97,36 @@ const AddQuestion = () => {
               <br />
               <br />
 
-              <label htmlFor="questionText"> <b>Question Text:</b> </label>
+              <label htmlFor="questionText">
+                <b>Question Text:</b>
+              </label>
               <input
                 type="text"
                 id="questionText"
                 value={questionText}
+                autoComplete="off"
                 onChange={(e) => setQuestionText(e.target.value)}
-                className="bg-gray-50 border mb-5 text-gray-900 text-sm rounded-lg block w-[70%] md:w-full p-2.5 "
+                className="bg-bgColor border border-green-950 text-gray-900 text-sm rounded-lg block w-full p-2.5 "
                 required
               />
 
+              <br />
+              <br />
+
+              <b>OPTIONS </b>
+
               {options.map((option, index) => (
                 <div key={index} className="text-base font-medium">
-                  <label htmlFor={`option${index}`}>Option {index + 1}:</label>
+                  {/* <label htmlFor={`option${index}`}>Option {index + 1}:</label> */}
                   <input
                     type="text"
                     id={`option${index}`}
                     value={option}
+                    placeholder={`Option ${index + 1}`}
                     onChange={(e) => handleOptionChange(index, e.target.value)}
-                    className="text-sm rounded-lg block w-[70%] md:w-full p-2.5"
+                    className="bg-bgColor border border-green-950 text-gray-900 text-sm rounded-lg block w-full p-2.5 "
                     required
+                    autoComplete="off"
                   />
                   <div className="mb-5 flex items-center">
                     <label htmlFor={`correctOption${index}`}>Correct</label>
@@ -129,7 +145,7 @@ const AddQuestion = () => {
 
               <label htmlFor="timeLimit">Time Limit (seconds):</label>
               <input
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-[70%] md:w-full p-2.5"
+                className="bg-bgColor border border-green-950 text-gray-900 text-sm rounded-lg block w-full p-2.5 "
                 type="number"
                 id="timeLimit"
                 value={timeLimit}
@@ -142,9 +158,9 @@ const AddQuestion = () => {
                 type="submit"
                 className={`w-full text-white flex justify-center mt-4  ${
                   loading
-                    ? "bg-[#22659c]"
-                    : "bg-primary-600 dark:bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300"
-                }  font-medium rounded-lg text-sm px-5 py-2.5 text-center  `}
+                    ? "bg-button"
+                    : "bg-button hover:bg-button focus:ring-4 focus:outline-none focus:ring-primary-300"
+                }  font-medium rounded-lg text-sm py-2.5 text-center  `}
               >
                 {loading ? <Adding /> : "Add Question"}
               </button>
