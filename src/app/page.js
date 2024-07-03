f0ea406ca35c5beeafb24049fa38afa95e19ee5c
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-07-03 20:28:58 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@ import Navbar from "@/components/Navbar";
 import CourseList from "@/components/admin/CourseList";
 
 export default function Home() {
-  
+
   return (<>
-  <Navbar/>
-   <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <CourseList  />
-    </main>
+    {/* <Navbar />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <CourseList />
+    </main> */}
   </>
-   
+
   );
 }
