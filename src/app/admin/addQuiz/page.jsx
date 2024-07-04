66746ebd73245493635c84b6296d3d8a2581ec4a
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-07-04 17:50:17 +0500

    Update page.jsx

--- src/app/admin/addQuiz/page.jsx
+++ src/app/admin/addQuiz/page.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const page = () => {
   return (
     <>
-      <div className="overflow-hidden w-dvw h-dvh bg-slate-50  bg-fixed bg-center" style={{ backgroundImage: "url('https://th.bing.com/th/id/OIP.NkwEpqJktRs1GVY_474xRQHaHa?w=184&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7')", backgroundSize: 'contain' }}>
+      <div className="overflow-hidden w-dvw h-dvh" >
         <AdminNavbar />
           <CreateQuizz />
       </div>
