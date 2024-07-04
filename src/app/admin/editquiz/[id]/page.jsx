5ab5daffe1eb1c7eb6515fab15736f0a2d274f5d
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-07-04 18:58:16 +0500

    Create page.jsx

---
+++ src/app/admin/editquiz/[id]/page.jsx
@@ -0,0 +1,12 @@
+import EditQuiz from '@/components/admin/EditQuiz'
+import React from 'react'
+
+const editQuiz = () => {
+  return (
+    <>
+    <EditQuiz />
+    </>
+  )
+}
+
+export default editQuiz
