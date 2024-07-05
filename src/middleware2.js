c3cb2f4ecd8106f9f8702a0ef381a901b123c366
MUDDUSSIR AWAN <126598592+MUDDUSSIR-RAZA@users.noreply.github.com>
2024-07-05 19:29:52 +0500

    Update middleware2.js

--- src/middleware2.js
+++ src/middleware2.js
@@ -19,7 +19,7 @@ export function middleware(request) {
     return;
 }
 
-// See "Matching Paths" below to learn mor
+// See "Mathing Paths" below to learn mor
 export const config = {
     matcher: [
         '/',
