477d0281041422ba38d0915361d1a0e89052f40b
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-07-06 20:29:55 +0500

    Create page.jsx

---
+++ src/app/student/EnterKey/page.jsx
@@ -0,0 +1,12 @@
+import EnterKeyPage from '@/components/Student/EnterKeyPage'
+import React from 'react'
+
+const EnterKey = () => {
+  return (
+    <>
+    <EnterKeyPage />
+    </>
+  )
+}
+
+export default EnterKey
