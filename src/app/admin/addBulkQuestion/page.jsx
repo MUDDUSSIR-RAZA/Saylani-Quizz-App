46c4845128ebf7ab87d69979571661e9f6fee8ce
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-07-06 20:28:45 +0500

    Update page.jsx

--- src/app/admin/addBulkQuestion/page.jsx
+++ src/app/admin/addBulkQuestion/page.jsx
@@ -1,4 +1,4 @@
-import AdminNavbar from "@/components/AdminNavbar";
+import AdminNavbar from "@/components/NavBar/AdminNavbar";
 import BulkUploadQuestions from "@/components/admin/BulkUploadQuestions";
 import React from "react";
 
