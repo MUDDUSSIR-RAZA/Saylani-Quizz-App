af6f647cc3add2f1f5049761036c272474e843d9
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-07-08 12:54:58 +0500

    Create page.jsx

---
+++ src/app/student/profile/page.jsx
@@ -0,0 +1,12 @@
+import ProfilePage from '@/components/Student/ProfilePage'
+import React from 'react'
+
+const profile = () => {
+  return (
+    <>
+    <ProfilePage />
+    </>
+  )
+}
+
+export default profile
