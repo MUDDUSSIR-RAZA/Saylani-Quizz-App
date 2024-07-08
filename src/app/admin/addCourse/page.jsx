fd0dbe89f4bb9fdc1da9bf62705cbba6a8652c05
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-07-08 12:54:56 +0500

    Create page.jsx

---
+++ src/app/admin/addCourse/page.jsx
@@ -0,0 +1,12 @@
+import AddCoursePage from '@/components/admin/AddCoursePage'
+import React from 'react'
+
+const addCourse = () => {
+  return (
+    <>
+    <AddCoursePage/>
+    </>
+  )
+}
+
+export default addCourse
