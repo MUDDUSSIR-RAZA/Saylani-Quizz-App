136a603d10976a01c68d3af49cc4f2afb4ec5be0
MUDDUSSIR AWAN <126598592+MUDDUSSIR-RAZA@users.noreply.github.com>
2024-07-11 22:34:54 +0500

    Update page.jsx

--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -1,1 +1,12 @@
-<></>
+import EnterKeyPage from '@/components/Student/EnterKeyPage'
+import React from 'react'
+
+const EnterKey = () => {
+  return (
+    <>
+    <EnterKeyPage />
+    </>
+  )
+}
+
+export default EnterKey
