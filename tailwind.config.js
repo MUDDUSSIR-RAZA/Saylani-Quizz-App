b3533834c71015807be567bbb0765e2ec5b685c0
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2024-07-13 01:32:43 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,13 +48,13 @@ module.exports = {
 
       fontFamily: {
         'body': [
+          'Roboto',
           'Inter',
           'ui-sans-serif',
           'system-ui',
           '-apple-system',
           'system-ui',
           'Segoe UI',
-          'Roboto',
           'Helvetica Neue',
           'Arial',
           'Noto Sans',
