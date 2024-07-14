afe777a322680059840d2ca9b3d4147bc59b8a77
MR Dev <muddussir1247@gmail.com>
2024-07-14 01:39:05 -0700

    Create AnimatedProgressProvider.jsx

---
+++ src/components/AnimatedProgressProvider.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Animate } from "react-move";
+
+const AnimatedProgressProvider = ({ valueStart, valueEnd, duration, easingFunction, children }) => (
+  <Animate
+    start={() => ({ value: valueStart })}
+    update={() => ({
+      value: [valueEnd],
+      timing: { duration: duration * 500, ease: easingFunction },
+    })}
+  >
+    {({ value }) => children(value)}
+  </Animate>
+);
+
+export default AnimatedProgressProvider;
