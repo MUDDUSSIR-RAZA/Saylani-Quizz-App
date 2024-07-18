b82ccea422c9024f28e0dc216ea204501347c580
MR Dev <muddussir1247@gmail.com>
2024-07-18 04:05:40 -0700

    Update page.jsx

--- src/yupSchemas/page.jsx
+++ src/yupSchemas/page.jsx
@@ -3,6 +3,13 @@ import * as Yup from "yup"
 export const loginSchema = Yup.object({
     email:Yup.string().email().required("Please Enter Your Email"),
     password:Yup.string().min(13).max(13).required("Please Enter Your Password"),
+    name: Yup.string().min(2).max(25).required("Please Enter Your Name"),
+    fathername: Yup.string().min(2).max(25).required("Please Enter Your Name"),
+    nic: "",
+    phone: "",
+    city: "",
+    course_name: "",
+    batch: "",
 })
 
 
