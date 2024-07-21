8e5f3d9d38943017db60df05edf3ed8b1db6c745
MR Dev <muddussir1247@gmail.com>
2024-07-21 03:22:39 -0700

    Update route.jsx

--- src/app/api/admin/studentRequest/route.jsx
+++ src/app/api/admin/studentRequest/route.jsx
@@ -2,15 +2,16 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  try {
     try {
-        console.log("request api route");
-        // try {
-        //     const { data } = await axios.get(`${process.env.BACKEND_URL}/admin/studentRequests`)
-        //     return NextResponse.json(data)
-        // } catch (axiosError) {
-        //     return NextResponse.json(axiosError.response.data, { status: 400 })
-        // } 
+      const { data } = await axios.get(
+        `${process.env.BACKEND_URL}/admin/studentRequests`
+      );
+      return NextResponse.json("data");
     } catch (axiosError) {
-        // return NextResponse.json(axiosError.response.data , {status : 400} )
+      return NextResponse.json(axiosError.response.data, { status: 400 });
     }
+  } catch (axiosError) {
+    return NextResponse.json(axiosError.response.data, { status: 400 });
+  }
 }
