7f8c2dff4ff2cc75e5670ea6f2b9c99f16d88262
MR Dev <muddussir1247@gmail.com>
2024-07-21 03:22:43 -0700

    Update page.jsx

--- src/app/admin/requests/page.jsx
+++ src/app/admin/requests/page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use server";
 
-const requests = () => {
-  return (
-    <div>requests</div>
-  )
+import RequestPage from "@/components/admin/RequestPage";
+import axios from "axios";
+import React from "react";
+
+async function getRequests() {
+  try {
+    const { data } = await axios.get("http://localhost:3000/api/admin/studentRequest");
+    return {
+      props: {
+        data,
+        error: null,
+      },
+    };
+  } catch (axiosError) {
+    return {
+      props: {
+        data: null,
+        error: axiosError.message,
+      },
+    };
+  }
 }
 
-export default requests
+const requests = async () => {
+  const data = await getRequests()
+  return <RequestPage />;
+};
+
+export default requests;
