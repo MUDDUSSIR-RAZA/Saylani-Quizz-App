61262498d130bcc35bc159f10ca63564d551726b
MR Dev <muddussir1247@gmail.com>
2024-07-24 05:35:29 -0700

    Update page.jsx

--- src/yupSchemas/page.jsx
+++ src/yupSchemas/page.jsx
@@ -2,15 +2,19 @@ import * as Yup from "yup"
 
 export const loginSchema = Yup.object({
     email:Yup.string().email().required("Please Enter Your Email"),
-    password:Yup.string().min(13).max(13).required("Please Enter Your Password"),
-    name: Yup.string().min(2).max(25).required("Please Enter Your Name"),
-    fathername: Yup.string().min(2).max(25).required("Please Enter Your Name"),
-    nic: "",
-    phone: "",
-    city: "",
-    course_name: "",
-    batch: "",
+    password:Yup.string().required("Please Enter Your Password")
 })
+// export const loginSchema = Yup.object({
+//     email:Yup.string().email().required("Please Enter Your Email"),
+//     password:Yup.string().min(13).max(13).required("Please Enter Your Password"),
+//     name: Yup.string().min(2).max(25).required("Please Enter Your Name"),
+//     fathername: Yup.string().min(2).max(25).required("Please Enter Your Name"),
+//     nic: "",
+//     phone: "",
+//     city: "",
+//     course_name: "",
+//     batch: "",
+// })
 
 
 export const quizTitleSchema = Yup.object({
