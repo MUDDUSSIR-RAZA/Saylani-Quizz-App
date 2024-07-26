26c3bfa5554bb74762c19ba3cd473befcf2e43bf
MR Dev <muddussir1247@gmail.com>
2024-07-26 04:02:55 -0700

    Update page.jsx

--- src/app/admin/createquiz/page.jsx
+++ src/app/admin/createquiz/page.jsx
@@ -1,14 +1,30 @@
 import AdminNavbar from "@/components/NavBar/AdminNavbar";
 import CreateQuizz from "@/components/admin/CreateQuizz";
+import axios from "axios";
 
-const createquiz = () => {
-  
+async function getCourses() {
+  try {
+    const { data } = await axios.get(
+      `${process.env.BACKEND_URL}/admin/getCourses`
+    );
+    return {
+      props: data,
+    };
+  } catch (error) {
+    return {
+      props: error.message,
+    };
+  }
+}
+
+const createquiz = async () => {
+  const { props } = await getCourses();
 
   return (
     <>
       <div className="overflow-hidden w-dvw h-dvh" >
         <AdminNavbar />
-          <CreateQuizz />
+          <CreateQuizz Courses={props} />
       </div>
     </>
   );
