93c281958f7188a0e0fcdb469af55e88968e95de
MR Dev <muddussir1247@gmail.com>
2024-07-26 02:03:48 -0700

    Create route.jsx

---
+++ src/app/api/admin/courses/getCourses/route.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    try {
+      const { data } = await axios.get(
+        `${process.env.BACKEND_URL}/admin/getCourses`
+      );
+      return NextResponse.json(data)
+    } catch (error) {
+      return NextResponse.json(error.response.data, { status: 400 });
+    }
+  } catch (error) {
+    return NextResponse.json(axiosError.response.data, { status: 400 });
+  }
+}
