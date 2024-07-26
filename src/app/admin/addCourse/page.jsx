7e34d0585b7a2bee30d152a51a200fe17b406bc2
MR Dev <muddussir1247@gmail.com>
2024-07-26 02:03:42 -0700

    Update page.jsx

--- src/app/admin/addCourse/page.jsx
+++ src/app/admin/addCourse/page.jsx
@@ -1,11 +1,34 @@
+"use server"
+
 import AddCoursePage from '@/components/admin/AddCoursePage'
+import AdminNavbar from '@/components/NavBar/AdminNavbar';
+import axios from 'axios';
 import React from 'react'
 
-const addCourse = () => {
+async function getCourses() {
+  try {
+    const { data } = await axios.get(
+      `${process.env.BACKEND_URL}/admin/getCourses`
+    );
+    return {
+      props: data,
+    };
+  } catch (error) {
+    return {
+      props: error.message,
+    };
+  }
+}
+
+const addCourse = async () => {
+  const { props } = await getCourses();
   return (
     <>
-    <AddCoursePage/>
-    </>
+    <AdminNavbar />
+    <main className="items-center justify-between py-24 -z-30">
+      <AddCoursePage Courses={props} />;
+    </main>
+  </>
   )
 }
 
