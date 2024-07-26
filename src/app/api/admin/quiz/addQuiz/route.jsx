a2e4f36e45e3d2d6a2ac9b569e064eaa46334f72
MR Dev <muddussir1247@gmail.com>
2024-07-26 04:02:59 -0700

    Create route.jsx

---
+++ src/app/api/admin/quiz/addQuiz/route.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    const { course_name,quiz_name , key} = await req.json();
+    try {
+      const {data} = await axios.post(
+        `${process.env.BACKEND_URL}/admin/addQuiz`,
+        { course_name,quiz_name , key }
+      );
+      return NextResponse.json(data);
+    } catch (axiosError) {
+      return NextResponse.json(axiosError.response.data, { status: 400 });
+    }
+  } catch (axiosError) {
+    return NextResponse.json(axiosError.response.data, { status: 400 });
+  }
+}
