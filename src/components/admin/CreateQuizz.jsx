304909131da6fbd851aa66dd48502fee206a9974
MR Dev <muddussir1247@gmail.com>
2024-07-26 04:03:05 -0700

    Update CreateQuizz.jsx

--- src/components/admin/CreateQuizz.jsx
+++ src/components/admin/CreateQuizz.jsx
@@ -10,16 +10,13 @@ import LogingIn from "@/components/InPageLoader/LogingIn";
 import Creating from "../InPageLoader/Creating";
 
 const initialValues = {
-  quiz: "",
+  quiz_name: "",
+  key: "",
+  course_name: "",
 };
 
-const CreateQuizz = () => {
-  const [courses, setCourses] = useState([
-    { _id: "60d21b4667d0d8992e610c85", course_name: "Mathematics 101" },
-    { _id: "60d21b4667d0d8992e610c86", course_name: "Science Basics" },
-    { _id: "60d21b4667d0d8992e610c87", course_name: "History 101" },
-  ]);
-  const [selectedCourse, setselectedCourse] = useState("");
+const CreateQuizz = ({ Courses }) => {
+  const [courses, setCourses] = useState(Courses);
 
   const [loading, setLoading] = useState(false);
 
@@ -29,37 +26,33 @@ const CreateQuizz = () => {
     useFormik({
       initialValues,
       validationSchema: quizTitleSchema,
-      onSubmit: async ({ quiz }, action) => {
+      onSubmit: async (action) => {
         setLoading(true);
-        // try {
-        //   setLoading(true);
-        //   const { data } = await axios.post(
-        //     "/api/user/login",
-        //     {
-        //       email,
-        //       password,
-        //     },
-        //     {
-        //       withCredentials: true,
-        //     }
-        //   );
-        //   setLoading(false);
-        //   router.push("/");
-        //   action.resetForm();
-        //   return;
-        // } catch (error) {
-        //   setLoading(false);
-        //   toast.error(error.response.data);
-        //   return;
-        // }
+        try {
+          setLoading(true);
+          const { data } = await axios.post(
+            "/api/admin/quiz/addQuiz",
+            {...values},
+            {
+              withCredentials: true,
+            }
+          );
+          setLoading(false);
+          // router.push("/admin/dashboard");
+          action.resetForm();
+          toast.success(data);
+          return;
+        } catch (error) {
+          setLoading(false);
+          toast.error(error.response.data);
+          return;
+        }
       },
     });
 
   return (
     <>
-      <div>
-        <Toaster />
-      </div>
+          <Toaster position="top-right" reverseOrder={true} />
       <section className="w-vwh h-dvh flex">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 w-10/12 sm:px-0">
           <div className="w-10/12 backdrop-blur-2xl bg-[#ffffff00] shadow-2xl rounded-lg md:mt-0 sm:w-full xl:p-0">
@@ -81,15 +74,17 @@ const CreateQuizz = () => {
                     Select Course:
                   </label>
                   <select
-                    id="courseSelect"
-                    value={selectedCourse}
-                    onChange={(e) => setselectedCourse(e.target.value)}
+                    id="course_name"
+                    name="course_name"
+                    value={values.course_name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
                     required
                     className="bg-bgColor font-bold text-slate-800 border border-gray-300 rounded-sm"
                   >
                     <option value="">Select a Course</option>
                     {courses.map((course) => (
-                      <option key={course._id} value={course._id}>
+                      <option key={course._id} value={course.course_name}>
                         {course.course_name}
                       </option>
                     ))}
@@ -98,20 +93,20 @@ const CreateQuizz = () => {
 
                 <div>
                   <label
-                    htmlFor="quiz"
+                    htmlFor="quiz_name"
                     className="block mb-2 text-sm font-medium text-gray-900"
                   >
                     Quiz Name
                   </label>
                   <input
                     type="text"
-                    name="quiz"
-                    id="quiz"
+                    name="quiz_name"
+                    id="quiz_name"
                     className="bg-bgColor border border-green-950 text-gray-900 text-sm rounded-lg block w-full p-2.5 "
                     placeholder="Course Name"
                     required
                     autoComplete="off"
-                    value={values.quiz}
+                    value={values.quiz_name}
                     onChange={handleChange}
                     onBlur={handleBlur}
                   />
@@ -124,22 +119,22 @@ const CreateQuizz = () => {
 
                 <div>
                   <label
-                    htmlFor="addKey"
+                    htmlFor="key"
                     className="block mb-2 text-sm font-medium text-gray-900"
                   >
                     Add Key
                   </label>
                   <input
                     type="password"
-                    name="addKey"
-                    id="addKey"
+                    name="key"
+                    id="key"
                     className="bg-bgColor border border-green-950 text-gray-900 text-sm rounded-lg block w-full p-2.5 "
                     placeholder="Quiz Key *****"
                     required
                     autoComplete="off"
-                    // value={values.ad}
-                    // onChange={handleChange}
-                    // onBlur={handleBlur}
+                    value={values.key}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
                   />
                 </div>
                 <button
