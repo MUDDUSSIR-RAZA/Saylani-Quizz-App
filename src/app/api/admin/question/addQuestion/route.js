26148a6869b9c24a8d50bdac69ba903314df216d
MR Dev <muddussir1247@gmail.com>
2024-07-26 23:17:13 -0700

    Update route.js

--- src/app/api/admin/question/addQuestion/route.js
+++ src/app/api/admin/question/addQuestion/route.js
@@ -3,15 +3,14 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
     try {
-        const { selectedQuiz, question_text, options, correctAnswer, time_limit } = await req.json();
+        const { quizId, question_text, options, correctAnswer, time_limit } = await req.json();
         try {
             const { data } = await axios.post(
                 `${process.env.BACKEND_URL}/admin/addQuestion`,
-                { selectedQuiz, question_text, options, correctAnswer, time_limit }
+                { quizId, question_text, options, correctAnswer, time_limit }
             );
             return NextResponse.json(data);
         } catch (axiosError) {
-            console.log("🚀 ~ POST ~ axiosError:", axiosError)
             return NextResponse.json(axiosError.response.data, { status: 400 });
         }
     } catch (axiosError) {
