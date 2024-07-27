adcd5708c591b166b534549f5da2825aeb9837c8
MR Dev <muddussir1247@gmail.com>
2024-07-27 00:19:26 -0700

    Update route.jsx

--- src/app/api/admin/quiz/addQuiz/route.jsx
+++ src/app/api/admin/quiz/addQuiz/route.jsx
@@ -3,11 +3,11 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
   try {
-    const { course_name,quiz_name , key} = await req.json();
+    const { course_name, course_id, quiz_name, key } = await req.json();
     try {
-      const {data} = await axios.post(
+      const { data } = await axios.post(
         `${process.env.BACKEND_URL}/admin/addQuiz`,
-        { course_name,quiz_name , key }
+        { course_name, course_id, quiz_name, key }
       );
       return NextResponse.json(data);
     } catch (axiosError) {
