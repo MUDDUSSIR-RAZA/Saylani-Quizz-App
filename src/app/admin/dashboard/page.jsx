0cac0537a67d86afed2db07ae9adef53408309f1
MR Dev <muddussir1247@gmail.com>
2024-07-26 23:17:10 -0700

    Update page.jsx

--- src/app/admin/dashboard/page.jsx
+++ src/app/admin/dashboard/page.jsx
@@ -1,13 +1,31 @@
 import AdminNavbar from "@/components/NavBar/AdminNavbar";
-import QuizList from "@/components/admin/QuizList";
+import DashBoard from "@/components/admin/DashBoard";
 import React from "react";
 
-const dashboard = () => {
+async function getQuizzes() {
+  try {
+    const { data } = await axios.get(
+      `${process.env.BACKEND_URL}/admin/getQuizzes`
+    );
+   
+    return {
+      props: data,
+    };
+  } catch (error) {
+    return {
+      props: error.message,
+    };
+  }
+}
+
+
+const dashboard = async () => {
+  const { props } = await getQuizzes();
   return (
     <>
       <AdminNavbar />
       <main className="flex min-h-screen flex-col items-center justify-between py-24 px-5">
-        <QuizList />
+        <DashBoard quizzes={props} />
       </main>
     </>
   );
