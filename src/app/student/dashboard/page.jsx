80f7e59c81600d079ddee73693cbbcae92891330
MR Dev <muddussir1247@gmail.com>
2024-07-27 05:37:32 -0700

    Update page.jsx

--- src/app/student/dashboard/page.jsx
+++ src/app/student/dashboard/page.jsx
@@ -1,6 +1,6 @@
 
 import StudentNavbar from "@/components/NavBar/StudentNavbar";
-import QuizList from "@/components/Student/QuizList";
+import DashBoard from "@/components/Student/DashBoard";
 import React from "react";
 
 const dashboard = () => {
@@ -8,7 +8,7 @@ const dashboard = () => {
     <>
     <StudentNavbar />
       <main className="flex min-h-screen flex-col items-center justify-between py-24 px-5">
-        <QuizList />
+        <DashBoard />
       </main>
     </>
   );
