bbd231acb68218e9fdcbd68bb6a483ef6996d5e0
MR Dev <muddussir1247@gmail.com>
2024-07-27 03:54:00 -0700

    Create route.js

---
+++ src/app/api/admin/quiz/getQuizById/route.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get('id');
+    try {
+        try {
+            const { data } = await axios.get(
+                `${process.env.BACKEND_URL}/admin/getQuizById` , {
+                    params: { id }
+                  }
+            );
+            return NextResponse.json(data)
+        } catch (error) {
+            return NextResponse.json(error.response.data, { status: 400 });
+        }
+    } catch (error) {
+        return NextResponse.json(axiosError.response.data, { status: 400 });
+    }
+}
