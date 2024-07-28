035c12c79efbea672a6658a93b238c7c9e4db584
MR Dev <muddussir1247@gmail.com>
2024-07-28 02:07:29 -0700

    Update page.jsx

--- src/app/student/quiz/[id]/page.jsx
+++ src/app/student/quiz/[id]/page.jsx
@@ -11,9 +11,28 @@
 import QuizPage from "@/components/Student/QuizPage";
 import React from "react";
 
+async function getAllQuizzes(params, searchParams) {
+  console.log(params, searchParams)
+  // console.log(searchParams)
+
+  // try {
+  //   const { data } = await axios.get(
+  //     `${process.env.BACKEND_URL}/admin/getAllQuizzes`
+  //   );
+   
+  //   return {
+  //     props: data,
+  //   };
+  // } catch (error) {
+  //   return {
+  //     props: error.message,
+  //   };
+  // }
+}
+
+
 const quiz = ({ params, searchParams }) => {
-  console.log(params)
-  console.log(searchParams)
+  getAllQuizzes(params.id,searchParams.key)
   
   return (
     <>
