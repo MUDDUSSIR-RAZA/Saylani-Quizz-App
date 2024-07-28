3ad88b74d414896d70228b6ac85d2cebd09887c1
MR Dev <muddussir1247@gmail.com>
2024-07-27 23:06:39 -0700

    Update page.jsx

--- src/app/student/dashboard/page.jsx
+++ src/app/student/dashboard/page.jsx
@@ -1,12 +1,12 @@
-
 import StudentNavbar from "@/components/NavBar/StudentNavbar";
 import DashBoard from "@/components/Student/DashBoard";
+import axios from "axios";
 import React from "react";
 
 const dashboard = () => {
   return (
     <>
-    <StudentNavbar />
+      <StudentNavbar />
       <main className="flex min-h-screen flex-col items-center justify-between py-24 px-5">
         <DashBoard />
       </main>
