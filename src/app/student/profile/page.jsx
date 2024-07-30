98b3f8f4af823721e696e856858efebd28595f86
MR Dev <muddussir1247@gmail.com>
2024-07-30 13:27:51 +0500

    Update page.jsx

--- src/app/student/profile/page.jsx
+++ src/app/student/profile/page.jsx
@@ -1,10 +1,13 @@
+import StudentNavbar from '@/components/NavBar/StudentNavbar'
 import ProfilePage from '@/components/Student/ProfilePage'
-import React from 'react'
 
 const profile = () => {
   return (
     <>
-    <ProfilePage />
+   <StudentNavbar />
+        <ProfilePage />
+      {/* <main className="flex min-h-screen flex-col items-center justify-between py-24 px-5">
+      </main> */}
     </>
   )
 }
