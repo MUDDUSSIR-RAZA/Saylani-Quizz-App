a8a6c2e905563f37afd9b827bec95416d3cdb862
MR Dev <muddussir1247@gmail.com>
2024-07-30 22:29:18 +0500

    Update QuizPage.jsx

--- src/components/Student/QuizPage.jsx
+++ src/components/Student/QuizPage.jsx
@@ -1,28 +1,5 @@
-// // pages/api/check-key.js
-
-// export default async function handler(req, res) {
-//   const { key, quizId } = req.body;
-
-//   // Dummy data for key validation
-//   const validKey = "dsasadassad545";
-//   const quizDetails = {
-//     id: "dsfdsf",
-//     quiz_name: "HTML",
-//     course: "Web and App Development",
-//     key: validKey,
-//     quiz: [
-//       // quiz questions
-//     ],
-//   };
-
-//   if (key === validKey && quizId === quizDetails.id) {
-//     res.status(200).json({ success: true, quizDetails });
-//   } else {
-//     res.status(401).json({ success: false, message: "Invalid key or quiz ID" });
-//   }
-// }
-
 "use client";
+
 import { useState, useEffect } from "react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
@@ -30,209 +7,8 @@ import { FaMedal } from "react-icons/fa";
 import { easeQuadInOut } from "d3-ease";
 import AnimatedProgressProvider from "../AnimatedProgressProvider";
 import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
-//   id: "dsfdsf",
-//   quiz_name: "HTML",
-//   course: "Web and App Development",
-//   key: "dsasadassad545",
-//   quiz: [
-//     {
-//       correct_answer: "3",
-//       options: ["1", "2", "3", "4"],
-//       question_text: "What is the length of the array [1, 2, 3, 4]?",
-//       time_limit: 30,
-//       author_id: "fsd2f454f5sfddsf",
-//       id: "dsfdsfsf534dsfds",
-//     },
-//     {
-//       correct_answer: "splice",
-//       options: ["slice", "splice", "split", "join"],
-//       question_text:
-//         "Which method can be used to remove elements from an array?",
-//       time_limit: 30,
-//       author_id: "fsd2f454f5sfddsf",
-//       id: "dsfdsfsf534dsfdg",
-//     },
-//     {
-//       correct_answer: "map",
-//       options: ["filter", "reduce", "map", "forEach"],
-//       question_text:
-//         "Which array method creates a new array populated with the results of calling a provided function on every element in the calling array?",
-//       time_limit: 30,
-//       author_id: "fsd2f454f5sfddsf",
-//       id: "dsfdsfsf534dsfdh",
-//     },
-//     {
-//       correct_answer: "0",
-//       options: ["0", "1", "undefined", "null"],
-//       question_text: "What is the index of the first element in an array?",
-//       time_limit: 30,
-//       author_id: "fsd2f454f5sfddsf",
-//       id: "dsfdsfsf534dsfdi",
-//     },
-//     {
-//       correct_answer: "filter",
-//       options: ["find", "filter", "reduce", "some"],
-//       question_text:
-//         "Which method creates a new array with all elements that pass the test implemented by the provided function?",
-//       time_limit: 30,
-//       author_id: "fsd2f454f5sfddsf",
-//       id: "dsfdsfsf534dsfdj",
-//     },
-//   ],
-// };
-// const quizDetails = {
-//   "_id": "66a549757a81111e8fb85796",
-//   "course": "66a549577a81111e8fb8578f",
-//   "questions": [
-//       {
-//           "_id": "66a60a7971a18ce726fc255e",
-//           "quiz": "66a549757a81111e8fb85796",
-//           "question_text": "Who wrote 'Romeo and Juliet'?",
-//           "options": [
-//               "Charles Dickens",
-//               "William Shakespeare",
-//               "Mark Twain",
-//               "Jane Austen"
-//           ],
-//           "correct_answer": "William Shakespeare",
-//           "time_limit": 30,
-//           "__v": 0
-//       },
-//       {
-//           "_id": "66a60a7971a18ce726fc2576",
-//           "quiz": "66a549757a81111e8fb85796",
-//           "question_text": "Which element has the atomic number 1?",
-//           "options": [
-//               "Helium",
-//               "Oxygen",
-//               "Hydrogen",
-//               "Carbon"
-//           ],
-//           "correct_answer": "Hydrogen",
-//           "time_limit": 30,
-//           "__v": 0
-//       },
-//       {
-//           "_id": "66a60a7971a18ce726fc255a",
-//           "quiz": "66a549757a81111e8fb85796",
-//           "question_text": "Which planet is known as the Red Planet?",
-//           "options": [
-//               "Earth",
-//               "Mars",
-//               "Jupiter",
-//               "Saturn"
-//           ],
-//           "correct_answer": "Mars",
-//           "time_limit": 30,
-//           "__v": 0
-//       },
-//       {
-//           "_id": "66a60a7971a18ce726fc256a",
-//           "quiz": "66a549757a81111e8fb85796",
-//           "question_text": "What is the chemical symbol for gold?",
-//           "options": [
-//               "Ag",
-//               "Au",
-//               "Gd",
-//               "Ga"
-//           ],
-//           "correct_answer": "Au",
-//           "time_limit": 30,
-//           "__v": 0
-//       },
-//       {
-//           "_id": "66a60a7971a18ce726fc2572",
-//           "quiz": "66a549757a81111e8fb85796",
-//           "question_text": "Who painted the Mona Lisa?",
-//           "options": [
-//               "Vincent van Gogh",
-//               "Leonardo da Vinci",
-//               "Pablo Picasso",
-//               "Claude Monet"
-//           ],
-//           "correct_answer": "Leonardo da Vinci",
-//           "time_limit": 30,
-//           "__v": 0
-//       },
-//       {
-//           "_id": "66a60a7971a18ce726fc2556",
-//           "quiz": "66a549757a81111e8fb85796",
-//           "question_text": "What is 2 + 2?",
-//           "options": [
-//               "3",
-//               "4",
-//               "5",
-//               "6"
-//           ],
-//           "correct_answer": "4",
-//           "time_limit": 30,
-//           "__v": 0
-//       },
-//       {
-//           "_id": "66a60a7971a18ce726fc2562",
-//           "quiz": "66a549757a81111e8fb85796",
-//           "question_text": "What is the largest ocean on Earth?",
-//           "options": [
-//               "Atlantic Ocean",
-//               "Indian Ocean",
-//               "Arctic Ocean",
-//               "Pacific Ocean"
-//           ],
-//           "correct_answer": "Pacific Ocean",
-//           "time_limit": 30,
-//           "__v": 0
-//       },
-//       {
-//           "_id": "66a60a7971a18ce726fc256e",
-//           "quiz": "66a549757a81111e8fb85796",
-//           "question_text": "How many continents are there on Earth?",
-//           "options": [
-//               "5",
-//               "6",
-//               "7",
-//               "8"
-//           ],
-//           "correct_answer": "7",
-//           "time_limit": 30,
-//           "__v": 0
-//       },
-//       {
-//           "_id": "66a60a7971a18ce726fc2566",
-//           "quiz": "66a549757a81111e8fb85796",
-//           "question_text": "What is the square root of 64?",
-//           "options": [
-//               "6",
-//               "7",
-//               "8",
-//               "9"
-//           ],
-//           "correct_answer": "8",
-//           "time_limit": 30,
-//           "__v": 0
-//       },
-//       {
-//           "_id": "66a60a7971a18ce726fc2552",
-//           "quiz": "66a549757a81111e8fb85796",
-//           "question_text": "What is the capital of France?",
-//           "options": [
-//               "Berlin",
-//               "Madrid",
-//               "Paris",
-//               "Lisbon"
-//           ],
-//           "correct_answer": "Paris",
-//           "time_limit": 30,
-//           "__v": 0
-//       }
-//   ],
-//   "course_name": "Web And App",
-//   "quiz_name": "CSS",
-//   "displayQuestions": "10",
-//   "quizOpen": true,
-//   "key": "123456891",
-//   "__v": 0
-// }
 const QuizPage = ({ quizId }) => {
   const [quizDetails, setQuizDetails] = useState({});
   const [quizStarted, setQuizStarted] = useState(false);
@@ -340,8 +116,8 @@ const QuizPage = ({ quizId }) => {
   useEffect(() => {
     const handleVisibilityChange = () => {
       if (document.hidden && quizStarted && !isQuizCompleted) {
-        // Automatically submit the quiz with a score of 0
-        submitResultWithZeroScore();
+        setScore(0);
+        submitResult();
         setIsQuizCompleted(true);
       }
     };
@@ -407,64 +183,18 @@ const QuizPage = ({ quizId }) => {
 
   const submitResult = async () => {
     try {
-      //   const response = await fetch("/api/submit-result", {
-      //     method: "POST",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //     },
-      //     body: JSON.stringify({
-      //       userId,
-      //       course_name,
-      //       quiz_name,
-      //       quizId: quizDetails.id,
-      //     }),
-      //   });
-      //   if (response.ok) {
-      //   } else {
-      //     console.error("Error submitting result");
-      //   }
-      console.log(
-        quizDetails.userId,
-        quizDetails.course_name,
-        quizDetails.course.batch,
-        quizDetails.quiz_name,
-        quizDetails.questions.length,
+      const { data } = await axios.post("/api/student/quiz/submitResult", {
+        userId: quizDetails.userId,
+        course_name: quizDetails.course_name,
+        batch: quizDetails.course.batch,
+        quiz_name: quizDetails.quiz_name,
+        totalQuestions: quizDetails.questions.length,
         score,
-      );
+      });
+      toast.success(data);
     } catch (error) {
-      // console.error("Error submitting result:", error);
-    }
-  };
-
-  const submitResultWithZeroScore = async () => {
-    // Prepare userAnswers with empty strings for all questions
-    const updatedUserAnswers = {};
-    quizDetails.questions.forEach((question) => {
-      updatedUserAnswers[question.id] = ""; // Set all answers to empty string
-    });
-
-    // Submit the quiz with a score of 0 and updated userAnswers
-    setScore(0);
-    setUserAnswers(updatedUserAnswers); // Update userAnswers state with empty answers
-
-    try {
-      // const response = await fetch("/api/submit-result", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({
-      //     userAnswers: updatedUserAnswers,
-      //     score: 0,
-      //     quizId: quizDetails.id,
-      //   }),
-      // });
-      // if (response.ok) {
-      // } else {
-      //   console.error("Error submitting result with 0 score");
-      // }
-    } catch (error) {
-      // console.error("Error submitting result with 0 score:", error);
+      console.log(error.response.data);
+      toast.error(error.response.data);
     }
   };
 
@@ -513,6 +243,7 @@ const QuizPage = ({ quizId }) => {
 
     return (
       <div className="flex justify-center flex-col items-center h-screen backdrop-blur-xl bg-bgColor rounded-lg shadow-2xl p-7">
+        <Toaster position="top-right" reverseOrder={true} />
         <h1 className="text-3xl font-bold mb-5">Quiz Completed!</h1>
         <div style={{ width: "150px", height: "150px" }}>
           <AnimatedProgressProvider
@@ -563,6 +294,7 @@ const QuizPage = ({ quizId }) => {
     <>
       {isPaused && (
         <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
+          <Toaster position="top-right" reverseOrder={true} />
           <div className="bg-white p-8 rounded-lg shadow-lg">
             <h2 className="text-xl font-bold mb-4">Quiz Paused</h2>
             <p>Please return to full-screen mode to continue the quiz.</p>
@@ -590,6 +322,7 @@ const QuizPage = ({ quizId }) => {
 
       {!isPaused && (
         <div className="w-dvw h-dvh flex justify-center items-center md:mt-10">
+          <Toaster position="top-right" reverseOrder={true} />
           <div className="flex flex-col items-center backdrop-blur-xl bg-bgColor rounded-lg shadow-2xl p-7">
             <h1 className="text-2xl font-bold tracking-widest mb-5">
               {quizDetails.quiz_name}
