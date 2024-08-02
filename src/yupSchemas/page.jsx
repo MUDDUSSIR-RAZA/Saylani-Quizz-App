bb4f8e202fa3cc948d52cd4728e00d2ae8328b1a
MR Dev <muddussir1247@gmail.com>
2024-08-02 21:41:40 +0500

    Update page.jsx

--- src/yupSchemas/page.jsx
+++ src/yupSchemas/page.jsx
@@ -18,6 +18,5 @@ export const loginSchema = Yup.object({
 
 export const quizTitleSchema = Yup.object({
   quiz_name: Yup.string().required("Please Enter Quiz Title"),
-  key: Yup.string().required("Please Enter Quiz Title"),
   course_name: Yup.string().required("Course selection is required"),
 });
