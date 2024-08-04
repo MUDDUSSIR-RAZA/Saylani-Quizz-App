66c3fc44a888e9681a7e4be45a6d4d4715eb92ae
MR Dev <muddussir1247@gmail.com>
2024-08-04 21:05:21 +0500

    Update route.js

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -10,9 +10,9 @@ export async function POST(req) {
                 email,
                 password,
             });
-            // const response = NextResponse.json({ message: "Login Succesfuly", success: true })
-            // response.cookies.set("token", data, { httpOnly: true })
-            // return response
+            const response = NextResponse.json({ message: "Login Succesfuly", success: true })
+            response.cookies.set("token", data, { httpOnly: true })
+            return response
         } catch (axiosError) {
             return NextResponse.json(axiosError.response.data, { status: 400 })
         }
