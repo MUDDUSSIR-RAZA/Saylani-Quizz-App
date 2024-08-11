8345ec5bfacba8d18153fbfccc70b6dddeaf3ba3
MR Dev <muddussir1247@gmail.com>
2024-08-11 06:54:19 +0500

    Update route.js

--- src/app/api/admin/quiz/editQuiz/route.js
+++ src/app/api/admin/quiz/editQuiz/route.js
@@ -1,19 +1,19 @@
-import axios from "axios";
-import { NextResponse } from "next/server";
+    import axios from "axios";
+    import { NextResponse } from "next/server";
 
-export async function PATCH(req) {
-    try {
-        const { _id, quiz_name, key, displayQuestions, quizOpen } = await req.json();
+    export async function PATCH(req) {
         try {
-            const { data } = await axios.patch(
-                `${process.env.BACKEND_URL}/admin/editQuiz`,
-                { _id, quiz_name, key, displayQuestions, quizOpen }
-            );
-            return NextResponse.json(data);
+            const { _id, quiz_name, key, displayQuestions, quizOpen } = await req.json();
+            try {
+                const { data } = await axios.patch(
+                    `${process.env.BACKEND_URL}/admin/editQuiz`,
+                    { _id, quiz_name, key, displayQuestions, quizOpen }
+                );
+                return NextResponse.json(data);
+            } catch (axiosError) {
+                return NextResponse.json(axiosError.response.data, { status: 400 });
+            }
         } catch (axiosError) {
             return NextResponse.json(axiosError.response.data, { status: 400 });
         }
-    } catch (axiosError) {
-        return NextResponse.json(axiosError.response.data, { status: 400 });
     }
-}
