cd6d0ad7bb81b3921545e0e68c276b9bd95c470c
MR Dev <muddussir1247@gmail.com>
2024-08-11 21:58:41 +0500

    Update page.jsx

--- src/app/admin/dashboard/page.jsx
+++ src/app/admin/dashboard/page.jsx
@@ -10,7 +10,8 @@ const dashboard = async () => {
     <>
       <AdminNavbar />
       <main className="flex min-h-screen flex-col items-center justify-between py-24 px-5">
-        <DashBoard />
+        <DashBoard 
+        />
       </main>
     </>
   );
