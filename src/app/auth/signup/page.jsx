87d47ffcfba14d3d63801b37f599d9bbe1c1ec12
MR Dev <muddussir1247@gmail.com>
2024-08-11 22:10:30 +0500

    Update page.jsx

--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -1,26 +1,10 @@
 import SignupPage from "@/components/auth/SignupPage"
-import axios from "axios";
-
-async function getCourses() {
-  try {
-    const { data } = await axios.get(
-      `${process.env.BACKEND_URL}/admin/getCourses`
-    );
-    return {
-      props: data,
-    };
-  } catch (error) {
-    return {
-      props: error.message,
-    };
-  }
-}
 
 const signup = async () => {
-  const { props } = await getCourses();
+  
   return (
     <>
-    <SignupPage Courses={props} />
+    <SignupPage />
     </>
   )
 }
