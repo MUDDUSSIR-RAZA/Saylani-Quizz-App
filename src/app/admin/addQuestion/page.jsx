5edc2e4765a5b584332295a36acfef9fd765626d
MR Dev <muddussir1247@gmail.com>
2024-08-11 21:54:25 +0500

    Update page.jsx

--- src/app/admin/addQuestion/page.jsx
+++ src/app/admin/addQuestion/page.jsx
@@ -2,21 +2,6 @@ import AdminNavbar from "@/components/NavBar/AdminNavbar";
 import AddQuestion from "@/components/admin/AddQuestion";
 import axios from "axios";
 import React from "react";
-
-async function getAllQuizzes() {
-  try {
-    const { data } = await axios.get(
-      `${process.env.BACKEND_URL}/admin/getAllQuizzes`
-    );
-   
-    return {
-      props: data,
-    };
-  } catch (error) {
-    return {
-      props: error.message,
-    };
-  }
 }
 
 
