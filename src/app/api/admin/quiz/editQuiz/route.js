ac7aa463aa528e9e7368578432b087b6aad43f82
MR Dev <muddussir1247@gmail.com>
2024-08-11 06:25:40 +0500

    Update route.js

--- src/app/api/admin/quiz/editQuiz/route.js
+++ src/app/api/admin/quiz/editQuiz/route.js
@@ -3,11 +3,11 @@ import { NextResponse } from "next/server";
 
 export async function PATCH(req) {
     try {
-        const { _id, quiz_name, key, displayQuestions } = await req.json();
+        const { _id, quiz_name, key, displayQuestions, quizOpen } = await req.json();
         try {
             const { data } = await axios.patch(
                 `${process.env.BACKEND_URL}/admin/editQuiz`,
-                { _id, quiz_name, key, displayQuestions }
+                { _id, quiz_name, key, displayQuestions, quizOpen }
             );
             return NextResponse.json(data);
         } catch (axiosError) {
