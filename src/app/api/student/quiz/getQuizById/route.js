a200b9db461989976390177cb68831d50433ad1e
MR Dev <muddussir1247@gmail.com>
2024-08-11 22:36:06 +0500

    Update route.js

--- src/app/api/student/quiz/getQuizById/route.js
+++ src/app/api/student/quiz/getQuizById/route.js
@@ -15,7 +15,6 @@ export async function GET(req) {
             );
             return NextResponse.json(data)
         } catch (error) {
-            console.log(error.response.data)
             return NextResponse.json(error.response.data, { status: 400 });
         }
     } catch (error) {
