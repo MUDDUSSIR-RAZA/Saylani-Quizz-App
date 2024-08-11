2f54105b87a881d7c8a7bbaa41fb3a20e452ee0d
MR Dev <muddussir1247@gmail.com>
2024-08-11 06:43:37 +0500

    Update AdminNavbar.jsx

--- src/components/NavBar/AdminNavbar.jsx
+++ src/components/NavBar/AdminNavbar.jsx
@@ -3,7 +3,7 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState, useEffect, } from "react";
+import { useState, useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const AdminNavbar = () => {
