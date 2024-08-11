9be747eca49b9409c66dc73f515fb26309af2bc9
MR Dev <muddussir1247@gmail.com>
2024-08-11 17:02:07 +0500

    Create page.jsx

---
+++ src/app/auth/admin/signup/page.jsx
@@ -0,0 +1,12 @@
+import AdminSignupPage from "@/components/auth/AdminSignupPage";
+import SignupPage from "@/components/auth/SignupPage"
+
+const signup = async () => {
+  return (
+    <>
+    <AdminSignupPage />
+    </>
+  )
+}
+
+export default signup
