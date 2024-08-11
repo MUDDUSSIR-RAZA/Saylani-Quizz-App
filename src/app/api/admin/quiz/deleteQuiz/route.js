b5283be429a9b9f8dd2e8f3ade7e0029bb63e47a
MR Dev <muddussir1247@gmail.com>
2024-08-11 09:38:40 +0500

    Update route.js

--- src/app/api/admin/quiz/deleteQuiz/route.js
+++ src/app/api/admin/quiz/deleteQuiz/route.js
@@ -2,18 +2,17 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-    console.log("first")
     try {
         const { _id } = await req.json();
-        // try {
-        //     const { data } = await axios.patch(
-        //         `${process.env.BACKEND_URL}/admin/editQuiz`,
-        //         { _id, quiz_name, key, displayQuestions, quizOpen }
-        //     );
-        return NextResponse.json("data");
-        // } catch (axiosError) {
-        //     return NextResponse.json(axiosError.response.data, { status: 400 });
-        // }
+        try {
+            const { data } = await axios.post(
+                `${process.env.BACKEND_URL}/admin/deleteQuiz`,
+                { _id }
+            );
+        return NextResponse.json(data);
+        } catch (axiosError) {
+            return NextResponse.json(axiosError.response.data, { status: 400 });
+        }
     } catch (axiosError) {
         return NextResponse.json("axiosError.response.data", { status: 400 });
     }
