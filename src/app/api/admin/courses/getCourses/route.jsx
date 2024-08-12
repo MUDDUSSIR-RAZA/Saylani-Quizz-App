a85ecf152af383c5834e63048302492f9f670928
MR Dev <muddussir1247@gmail.com>
2024-08-12 20:28:02 +0500

    Update route.jsx

--- src/app/api/admin/courses/getCourses/route.jsx
+++ src/app/api/admin/courses/getCourses/route.jsx
@@ -5,7 +5,7 @@ export async function GET() {
   try {
     try {
       const { data } = await axios.get(
-        `${process.env.BACKEND_URL}/admin/getCourses`
+        `https://saylani-quizz-app-backend.onrender.com/admin/getCourses`
       );
       console.log("data " , data)
       return NextResponse.json(data)
