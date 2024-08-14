d04c2870759a45d4bffadddec72084331a9b9c24
MR Dev <muddussir1247@gmail.com>
2024-08-14 18:43:42 +0500

    Update route.jsx

--- src/app/api/admin/courses/getCourses/route.jsx
+++ src/app/api/admin/courses/getCourses/route.jsx
@@ -5,17 +5,12 @@ export async function GET() {
   console.log("Server Get Request");
   try {
     console.log("Server Get Request");
-    try {
-      console.log("Server Get Request");
-      const { data } = await axios.get(
-        `https://saylani-quizz-app-backend.onrender.com/admin/getCourses`
-      );
-      console.log("Server Get Request Response", data);
-      return NextResponse.json(data);
-    } catch (error) {
-      return NextResponse.json(error.response.data, { status: 400 });
-    }
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`
+    );
+    console.log("Server Get Request Response", data);
+    return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json(axiosError.response.data, { status: 400 });
+    return NextResponse.json(error.response.data, { status: 400 });
   }
 }
