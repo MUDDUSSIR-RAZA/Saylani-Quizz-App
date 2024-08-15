4b955f04ed629412dcbae5afb28d06cb1cd3b7d7
MR Dev <muddussir1247@gmail.com>
2024-08-15 18:48:43 +0500

    Update route.jsx

--- src/app/api/admin/courses/getCourses/route.jsx
+++ src/app/api/admin/courses/getCourses/route.jsx
@@ -9,7 +9,11 @@ export async function GET() {
       `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`
     );
     console.log("Server Get Request Response", data);
-    return NextResponse.json(data);
+    return NextResponse.json(data, {
+      headers: {
+        'Cache-Control': 'no-store',
+      },
+    });
   } catch (error) {
     return NextResponse.json(error.response.data, { status: 400 });
   }
