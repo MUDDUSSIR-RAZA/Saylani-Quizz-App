2c4509358a56701af9706bf1bbdd786afa53f22b
MR Dev <muddussir1247@gmail.com>
2024-08-16 00:12:42 +0500

    Update route.jsx

--- src/app/api/admin/courses/getCourses/route.jsx
+++ src/app/api/admin/courses/getCourses/route.jsx
@@ -2,19 +2,16 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  // const { data } = await axios.get(
-  //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`
-  // );
   console.log("Server Get Request");
   try {
     console.log("Server Get Request");
-    const data = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`, {
-      method: "GET",
-      cache: "no-store",
-    });
-    // const data = await res.json(); // Assign response to `data` variable
-  
-    // Now you can access `data` safely:
+    // const { data } = await axios.get(
+    //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`
+    // );
+    const res = await fetch("/api/admin/courses/getCourses", {
+          cache: "no-store",
+        });
+        const data = await res.json();
     console.log("Server Get Request Response", data);
     return NextResponse.json(data);
   } catch (error) {
