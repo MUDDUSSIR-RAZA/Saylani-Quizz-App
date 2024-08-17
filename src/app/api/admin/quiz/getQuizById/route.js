9bbfe65a7050eb052e11452b00f5d592f213573c
MR Dev <muddussir1247@gmail.com>
2024-08-17 09:52:25 +0500

    Update route.js

--- src/app/api/admin/quiz/getQuizById/route.js
+++ src/app/api/admin/quiz/getQuizById/route.js
@@ -12,6 +12,6 @@ export async function GET(req) {
         const data = await res.json();
         return NextResponse.json(data)
     } catch (error) {
-        return NextResponse.json(error.response.data, { status: 400 });
+        return NextResponse.json(error, { status: 400 });
     }
 }
