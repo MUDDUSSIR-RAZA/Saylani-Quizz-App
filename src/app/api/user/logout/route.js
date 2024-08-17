a73053359b66e348e2c048f7eda457c08d59d580
MR Dev <muddussir1247@gmail.com>
2024-08-17 12:35:54 +0500

    Update route.js

--- src/app/api/user/logout/route.js
+++ src/app/api/user/logout/route.js
@@ -2,20 +2,15 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
     try {
-        try {
+        const response = NextResponse.json({ message: 'Logout Successfully', success: true })
 
-            const response = NextResponse.json({ message: 'Logout Successfully', success: true })
+        response.cookies.set("token", "", {
+            httpOnly: true,
+            expires: new Date(0)
+        })
 
-            response.cookies.set("token", "", {
-                httpOnly: true,
-                expires: new Date(0)
-            })
-
-            return response
-        } catch (axiosError) {
-            return NextResponse.json(axiosError.response.data, { status: 400 })
-        }
+        return response
     } catch (axiosError) {
-        return NextResponse.json(error)
+        return NextResponse.json(axiosError.response.data, { status: 400 })
     }
 }
