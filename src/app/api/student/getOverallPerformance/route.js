e581c1080ee4f3803711bd92051cab7bc25fa548
MR Dev <muddussir1247@gmail.com>
2024-08-17 19:54:50 +0500

    Update route.js

--- src/app/api/student/getOverallPerformance/route.js
+++ src/app/api/student/getOverallPerformance/route.js
@@ -3,9 +3,7 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-
     const token = cookies().get("token").value;
-
     try {
         // const { data } = await axios.get(
         //     `${process.env.NEXT_PUBLIC_BACKEND_URL}/student/getOverallPerformance`, {
@@ -13,7 +11,7 @@ export async function GET(req) {
         // }
         // );
         const res = await fetch(
-            `process.env.NEXT_PUBLIC_BACKEND_URL}/student/getOverallPerformance?token=${token}`,
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/student/getOverallPerformance?token=${token}`,
             { next: { tags: ["performance"] }, cache: "no-store" }
         );
         const data = await res.json();
