7c38e43ac95490756e60163d8877a07deaa1f3d1
MR Dev <muddussir1247@gmail.com>
2024-08-17 09:43:16 +0500

    Update route.jsx

--- src/app/api/admin/courses/getCourses/route.jsx
+++ src/app/api/admin/courses/getCourses/route.jsx
@@ -5,17 +5,15 @@ export async function GET() {
   console.log("Server Get Request");
   try {
     console.log("Server Get Request");
-    // const { data } = await axios.get(
-    //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`, {
-    //     cache: "no-store",
-    //   }
-    // );
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`, { next: { tags: ['courses'] } , cache: "no-store"}
+    );
     // const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`, { next: { tags: ['courses'] } , cache: "no-store"});
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`, { next: { tags: ['courses'] } , cache: "no-store"});
-    const data = await res.json();
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`, { next: { tags: ['courses'] } , cache: "no-store"});
+    // const data = await res.json();
     console.log("Server Get Request Response", data);
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json(error, { status: 400 });
+    return NextResponse.json(error.response.data, { status: 400 });
   }
 }
