f437a65ffa2c53b9c3185226e37b9bada6a28434
MR Dev <muddussir1247@gmail.com>
2024-08-17 12:31:41 +0500

    Update route.jsx

--- src/app/api/admin/courses/addCourse/route.jsx
+++ src/app/api/admin/courses/addCourse/route.jsx
@@ -3,19 +3,15 @@ import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
+  const { course_name, batch, cities } = await req.json();
   try {
-    const { course_name, batch, cities } = await req.json();
-    try {
-      const {data} = await axios.post(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/addCourse`,
-        { course_name, batch, cities }
-      );
-      revalidateTag("courses")
-      console.log("Add Course API data " , data)
-      return NextResponse.json(data);
-    } catch (axiosError) {
-      return NextResponse.json(axiosError.response.data, { status: 400 });
-    }
+    const { data } = await axios.post(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/addCourse`,
+      { course_name, batch, cities }
+    );
+    revalidateTag("courses");
+    console.log("Add Course API data ", data);
+    return NextResponse.json(data);
   } catch (axiosError) {
     return NextResponse.json(axiosError.response.data, { status: 400 });
   }
