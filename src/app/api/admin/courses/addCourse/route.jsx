0e617e4328e4454d440c41da69492c4f6d451b75
MR Dev <muddussir1247@gmail.com>
2024-08-17 09:39:36 +0500

    Update route.jsx

--- src/app/api/admin/courses/addCourse/route.jsx
+++ src/app/api/admin/courses/addCourse/route.jsx
@@ -10,7 +10,7 @@ export async function POST(req) {
         `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/addCourse`,
         { course_name, batch, cities }
       );
-      // revalidateTag("courses")
+      revalidateTag("courses")
       console.log("Add Course API data " , data)
       return NextResponse.json(data);
     } catch (axiosError) {
