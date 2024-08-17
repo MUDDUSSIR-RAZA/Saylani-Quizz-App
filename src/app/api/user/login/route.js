87d574eb285314b3dfe4d1f94f56b8f9b1261197
MR Dev <muddussir1247@gmail.com>
2024-08-17 12:35:39 +0500

    Update route.js

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -2,21 +2,16 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-
+    const { email, password } = await req.json()
     try {
-        const { email, password } = await req.json()
-        try {
-            const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`, {
-                email,
-                password,
-            });
-            const response = NextResponse.json({ message: "Login Succesfuly", success: true })
-            response.cookies.set("token", data, { httpOnly: true })
-            return response
-        } catch (axiosError) {
-            return NextResponse.json(axiosError.response.data, { status: 400 })
-        }
+        const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`, {
+            email,
+            password,
+        });
+        const response = NextResponse.json({ message: "Login Succesfuly", success: true })
+        response.cookies.set("token", data, { httpOnly: true })
+        return response
     } catch (axiosError) {
-        return NextResponse.json(error)
+        return NextResponse.json(axiosError.response.data, { status: 400 })
     }
 }
