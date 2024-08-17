f0155ee1ba5462da2d7ffd387b9ea075821d5692
MR Dev <muddussir1247@gmail.com>
2024-08-17 12:35:25 +0500

    Update route.js

--- src/app/api/user/adminSignup/route.js
+++ src/app/api/user/adminSignup/route.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-
     try {
         const admin = await req.json()
         const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/admin/signUp`, {
