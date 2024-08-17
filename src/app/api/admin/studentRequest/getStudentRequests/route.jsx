5f351050c5393f05f9389a54a0d4cd72aca6d985
MR Dev <muddussir1247@gmail.com>
2024-08-17 12:33:47 +0500

    Update route.jsx

--- src/app/api/admin/studentRequest/getStudentRequests/route.jsx
+++ src/app/api/admin/studentRequest/getStudentRequests/route.jsx
@@ -3,20 +3,16 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    try {
-      // const { data } = await axios.get(
-      //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getStudentRequests`
-      // );
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`,
-        { next: { tags: ["requests"] }, cache: "no-store" }
-      );
-      const data = await res.json();
-      return NextResponse.json(data);
-    } catch (axiosError) {
-      return NextResponse.json(axiosError, { status: 400 });
-    }
+    // const { data } = await axios.get(
+    //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getStudentRequests`
+    // );
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`,
+      { next: { tags: ["requests"] }, cache: "no-store" }
+    );
+    const data = await res.json();
+    return NextResponse.json(data);
   } catch (axiosError) {
-    return NextResponse.json(axiosError.response.data, { status: 400 });
+    return NextResponse.json(axiosError, { status: 400 });
   }
 }
