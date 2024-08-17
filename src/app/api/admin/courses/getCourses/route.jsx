c303e928b375713d70d51dc1269ea008c1378093
MR Dev <muddussir1247@gmail.com>
2024-08-17 09:37:44 +0500

    Update route.jsx

--- src/app/api/admin/courses/getCourses/route.jsx
+++ src/app/api/admin/courses/getCourses/route.jsx
@@ -10,7 +10,8 @@ export async function GET() {
     //     cache: "no-store",
     //   }
     // );
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`, { next: { tags: ['courses'] } , cache: "no-store"});
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`, { next: { tags: ['courses'] } , cache: "no-store"});
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/getCourses`, { cache: "no-store"});
     const data = await res.json();
     console.log("Server Get Request Response", data);
     return NextResponse.json(data);
