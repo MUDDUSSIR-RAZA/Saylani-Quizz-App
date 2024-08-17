e8112215e71dac3ded6a1d33d933cfdaad000209
MR Dev <muddussir1247@gmail.com>
2024-08-17 12:34:46 +0500

    Update route.js

--- src/app/api/student/quiz/getStudentQuiz/route.js
+++ src/app/api/student/quiz/getStudentQuiz/route.js
@@ -5,19 +5,19 @@ import { NextResponse } from "next/server";
 export async function GET(req) {
 
     const token = cookies().get("token").value;
-        try {
-            // const { data } = await axios.get(
-            //     `${process.env.NEXT_PUBLIC_BACKEND_URL}/student/getStudentQuiz`, {
-            //     params: { token }
-            // }
-            // );
-            const res = await fetch(
-                `${process.env.NEXT_PUBLIC_BACKEND_URL}/student/getStudentQuiz?token=${token}`,
-                { next: { tags: ["quizzes"] }, cache: "no-store" }
-            );
-            const data = await res.json();
-            return NextResponse.json(data)
-        } catch (error) {
-            return NextResponse.json(error.response.data, { status: 400 });
-        }
+    try {
+        // const { data } = await axios.get(
+        //     `${process.env.NEXT_PUBLIC_BACKEND_URL}/student/getStudentQuiz`, {
+        //     params: { token }
+        // }
+        // );
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/student/getStudentQuiz?token=${token}`,
+            { next: { tags: ["quizzes"] }, cache: "no-store" }
+        );
+        const data = await res.json();
+        return NextResponse.json(data)
+    } catch (error) {
+        return NextResponse.json(error.response.data, { status: 400 });
+    }
 }
