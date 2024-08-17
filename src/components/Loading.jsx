c03bc8fbe71659a6fed2fe3eee215d50b2c46708
MR Dev <muddussir1247@gmail.com>
2024-08-17 09:47:59 +0500

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,7 @@ export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
     <>
-      <div className="w-dvw h-dvh flex justify-center items-center backdrop-blur-2xl bg-[#ffffff00] rounded-lg shadow-2xl">
+      <div className="flex justify-center items-center backdrop-blur-2xl bg-[#ffffff00] rounded-lg shadow-2xl">
         <div className={pl}>
           <div className={pl__dot}></div>
           <div className={pl__dot}></div>
