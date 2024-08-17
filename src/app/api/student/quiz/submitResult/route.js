f799a5abfea702d8feda7b3e7ef559912bd996dd
MR Dev <muddussir1247@gmail.com>
2024-08-17 12:31:21 +0500

    Update route.js

--- src/app/api/student/quiz/submitResult/route.js
+++ src/app/api/student/quiz/submitResult/route.js
@@ -1,14 +1,17 @@
 import axios from "axios";
+import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
   try {
-    const { userId, course_name , batch , quiz_name , totalQuestions , score } = await req.json();
+    const { userId, course_name, batch, quiz_name, totalQuestions, score } = await req.json();
     try {
-      const {data} = await axios.post(
+      const { data } = await axios.post(
         `${process.env.NEXT_PUBLIC_BACKEND_URL}/student/submitResult`,
-        {  userId, course_name , batch , quiz_name , totalQuestions , score  }
-      );return NextResponse.json(data);
+        { userId, course_name, batch, quiz_name, totalQuestions, score }
+      );
+      revalidateTag("performance")
+      return NextResponse.json(data);
     } catch (axiosError) {
       return NextResponse.json(axiosError.response.data, { status: 400 });
     }
