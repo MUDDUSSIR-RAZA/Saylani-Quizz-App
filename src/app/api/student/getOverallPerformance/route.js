984add6d38dc4bda41f0702af0f8e9bcce9cc917
MR Dev <muddussir1247@gmail.com>
2024-08-17 12:33:58 +0500

    Update route.js

--- src/app/api/student/getOverallPerformance/route.js
+++ src/app/api/student/getOverallPerformance/route.js
@@ -5,23 +5,21 @@ import { NextResponse } from "next/server";
 export async function GET(req) {
 
     const token = cookies().get("token").value;
+
     try {
-        try {
-            // const { data } = await axios.get(
-            //     `${process.env.NEXT_PUBLIC_BACKEND_URL}/student/getOverallPerformance`, {
-            //     params: { token }
-            // }
-            // );
-            const res = await fetch(
-                `process.env.NEXT_PUBLIC_BACKEND_URL}/student/getOverallPerformance?token=${token}`,
-                { next: { tags: ["performance"] }, cache: "no-store" }
-            );
-            const data = await res.json();
-            return NextResponse.json(data)
-        } catch (error) {
-            return NextResponse.json(error.response.data, { status: 400 });
-        }
+        // const { data } = await axios.get(
+        //     `${process.env.NEXT_PUBLIC_BACKEND_URL}/student/getOverallPerformance`, {
+        //     params: { token }
+        // }
+        // );
+        const res = await fetch(
+            `process.env.NEXT_PUBLIC_BACKEND_URL}/student/getOverallPerformance?token=${token}`,
+            { next: { tags: ["performance"] }, cache: "no-store" }
+        );
+        const data = await res.json();
+        return NextResponse.json(data)
     } catch (error) {
-        return NextResponse.json(axiosError.response.data, { status: 400 });
+        return NextResponse.json(error.response.data, { status: 400 });
     }
+
 }
