f8b580314a84aa8f76c5ec24520a7e6e9cb5b01a
MR Dev <muddussir1247@gmail.com>
2024-08-17 12:36:09 +0500

    Update route.js

--- src/app/api/user/signup/route.js
+++ src/app/api/user/signup/route.js
@@ -3,14 +3,10 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
     try {
-        const {studentData} = await req.json()
-        try {
-            const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/signUp`, {...studentData});
-            return NextResponse.json(data)
-        } catch (axiosError) {
-            return NextResponse.json(axiosError.response.data, { status: 400 })
-        }
+        const { studentData } = await req.json()
+        const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/signUp`, { ...studentData });
+        return NextResponse.json(data)
     } catch (axiosError) {
-        return NextResponse.json(error)
+        return NextResponse.json(axiosError.response.data, { status: 400 })
     }
 }
